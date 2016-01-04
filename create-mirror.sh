00a64c8986ea71355215226935a71c885f068d3f
dan <dan.straw@amoma.com>
2016-01-04 13:14:53 +0000

    Adding additional git commands to push to new origin

--- create-mirror.sh
+++ create-mirror.sh
@@ -17,3 +17,12 @@ git filter-branch -f --prune-empty --tag-name-filter cat --subdirectory-filter l
 # Add the extra text to indicate that this has been cloned.
 cat README.md | sed -e 's/^License$/This repository was cloned from https:\/\/github.com\/apache\/thrift using the script found in create-mirror.sh\n\nLicense/' > README.md.tmp
 mv README.md.tmp README.md
+
+# Rename the remote origin repository
+git remote rename origin origin-apache-thrift
+
+# Add the new origin
+git remote add origin git@github.com:amoma-com/thrift-php.git
+
+# Push everything to the new origin
+git push origin --all
